10e73312ced550fac0f1b5c849ec440ea74f1ce2
Charvi <charvikapoor1996@gmail.com>
2026-08-12 17:49:44 +0530

    upload

--- Python/python_users.py
+++
@@ -1,7 +0,0 @@
-import pickle
-with open('encodings.pkl', 'rb') as f:
-    encodeListKnown, classNames = pickle.load(f)
-
-print("Stored Users in encodings.pkl:")
-for i, name in enumerate(classNames):
-    print(f"{i + 1}. {name}")
